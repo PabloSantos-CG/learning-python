72c92cd3b05d4e12a627d41ce9dd5e0d5b063f38
PabloSantos-CG <comercial-pablo@outlook.com>
2025-01-06 09:49:00 -0300

    exercise

--- oop/ex02.py
+++ oop/ex02.py
@@ -1,14 +1,3 @@
-# Exercício com classes
-# 1 - Crie uma classe Carro (Nome)
-# 2 - Crie uma classe Motor (Nome)
-# 3 - Crie uma classe Fabricante (Nome)
-# 4 - Faça a ligação entre Carro tem um Motor
-# Obs.: Um motor pode ser de vários carros
-
-# 5 - Faça a ligação entre Carro e um Fabricante
-# Obs.: Um fabricante pode fabricar vários carros
-# Exiba o nome do carro, motor e fabricante na tela
-###
 class Car:
     def __init__(self, name):
         self._name = name
@@ -26,12 +15,12 @@ class Car:
     def engine(self, value):
         self._engine = value
 
-###
+
 class Engine:
     def __init__(self, name):
         self.name = name
 
-###
+
 class ManufactureCars:
     def __init__(self, name):
         self.name = name
@@ -42,7 +31,7 @@ class ManufactureCars:
 
     def show(self):
         for car in self._cars:
-            print(f'Nome: {car.name} \nMotor: {car.engine.name} \nFabricante: {self.name}')
+            print(f'\nNome: {car.name} \nMotor: {car.engine.name} \nFabricante: {self.name}')
 
 polo = Car("Polo")
 polo_engine = Engine("1.2")
